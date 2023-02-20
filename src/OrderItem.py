cfd2ce59aacc4c548fe0b061169b233b6bb1ba03
Christopher Williams <112411677+cwilliams4950@users.noreply.github.com>
2023-02-20 17:02:15 -0500

    Saving documentation
    This is the documentation for my project

--- src/OrderItem.py
+++ src/OrderItem.py
@@ -4,11 +4,21 @@ class OrderItem(object) :
     COUNTER_ID = 1
 
     id = 0
+    """! This field represents the amount of items"""
     amount = 0
+    """! This field represents the amount of items"""
     value = 0
+    """! This field represents the amount of items"""
     discount = 0.0
+    """! This field represents the discount of items"""
 
     def __init__(self, amount,  value,  discount):
+
+        """!Constructor
+        @param amount This represents the amount
+        @param amount This represents the value
+        @param amount This represents the discount
+        """
         
         self.id = OrderItem.COUNTER_ID
         
@@ -25,6 +35,12 @@ class OrderItem(object) :
         return "id=" + str(self.id) + ", amount=" + str(self.amount) + ", value=" + str(self.value) + ", discount=" + str(self.discount) + ", finalValue=" + str(self.getFinalValue())
 
     def isValid(self):
+
+        """! This method validates if an order item is valid or not.
+        This method determines if a value is within an acceptable range.
+        If amount and value are negatives, an order is invalid.
+        If the discount is less than 0 or greater than 1 then an order is invalid.
+        """
         
         if self.amount < 0.0: 
             return False
